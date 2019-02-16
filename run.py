c26d1bc8d5e7613fb857c07b204259b218b30414
Vinicius Figueiredo <mvcf10@gmail.com>
2019-02-16 14:24:52 -0300

    raspicamera

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
 from imutils.video import VideoStream
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
@@ -20,36 +29,42 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
-
-while True:
-	frame = vs.read()
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
 	frame = imutils.resize(frame, width=420)
 
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-		(300, 300), (104.0, 177.0, 123.0))
- 	net.setInput(blob)
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+	net.setInput(blob)
 	detections = net.forward()
 
 	for i in range(0, detections.shape[2]):
 		confidence = detections[0, 0, i, 2]
-	if confidence < args["confidence"]:
+		if confidence < args["confidence"]:
 			continue
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
  
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(frame, (startX, startY), (endX, endY),
-			(0, 0, 255), 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 0, 255), 2)
 		cv2.putText(frame, text, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-	cv2.imshow("Frame", frame)
+ 
+	# show the frame
+	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
  
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
 
 cv2.destroyAllWindows()
-vs.stop()
