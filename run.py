f20206ec19fc18b7fff2a00d0395c49b2dd2d17e
Vinicius Figueiredo <mvcf10@gmail.com>
2019-02-16 15:53:30 -0300

    new code

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# USAGE
+# python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
 
+# import the necessary packages
 from imutils.video import VideoStream
 import numpy as np
 import argparse
@@ -8,12 +9,7 @@ import imutils
 import time
 import cv2
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (300, 300)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(300, 300))
-
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
@@ -23,50 +19,72 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
+# load our serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
+# initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
- 
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	#frame = imutils.resize(frame, 300,300)
 
-	(h, w) = frame.shape
-	#model.setInput(cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True))
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
+vs = VideoStream(usePiCamera=1).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=420)
+ 
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+		(300, 300), (104.0, 177.0, 123.0))
+ 
+	# pass the blob through the network and obtain the detections and
+	# predictions
 	net.setInput(blob)
 	detections = net.forward()
 	c = 0
+	
+	# loop over the detections
 	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
 		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
 		if confidence < args["confidence"]:
 			continue
+		c = c+1
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
- 
+		
+		
+		# draw the bounding box of the face along with the associated
+		# probability
+		idx = len(box)		
+		
 		text = "{:.2f}%".format(confidence * 100)
+		
 		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 0, 255), 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
 		cv2.putText(frame, text, (startX, y),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-		c = c + 1
- 
-	# show the frame
-	#cv2.imshow("Frame", image)
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+	print(c)
+	# show the output frame
+	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
- 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
  
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-	print(c)
 
+# do a bit of cleanup
 cv2.destroyAllWindows()
+vs.stop()
